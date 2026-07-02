8a1f47765d50d327f6562764d52e9771cbd198b3
sjsiwat <sj.siwat@gmail.com>
2026-07-02 13:37:33 +0700

    add data array and object and render in html

---
+++ my-array.js
@@ -0,0 +1,7 @@
+let mycat = ["johny","bobby","johny jr","babybear"];
+
+console.log(mycat[0]); // Output: johny
+console.log(mycat[1]); // Output: bobby
+console.log(mycat[2]); // Output: johny jr
+console.log(mycat[3]); // Output: babybear
+
---
+++ my-object.js
@@ -0,0 +1,9 @@
+//*  Johny,Bobby,Johny Jr,Babybear
+
+let mycatgang = {
+    cat1: { name: "johny", color: "black", size: "big" },
+    cat2: { name: "bobby", color: "black and white", size: "medium" },
+    cat3: { name: "johny jr", color: "same as johny", size: "small" },
+    cat4: { name: "babybear", color: "brown", size: "tiny" }
+};
+
